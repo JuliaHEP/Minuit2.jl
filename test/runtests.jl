35919c1d03ec14dd2f86d1b830023cc857b860f2
Jan Strube <jan.strube@gmail.com>
2020-12-28 23:59:13 -0800

    First version of working test

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,9 @@ gaussian(pars, x) = pars[3] * exp(-0.5*(x-pars[1])^2/(pars[2]^2))/(√(2.0*π)*p
     x = collect(data.edges[1])[1:end-1]
     y = data.weights 
     var = ones(length(y))
-    init = [0.0, 1.0, 100.0]
+    parameters = [0.0, 1.0, 100.0]
     gaussFunc = @safe_cfunction(gaussian, Float64, (CxxWrap.StdLib.StdVectorAllocated{Float64}, Float64))
-    fitter = Minuit2.JuliaFcn(c_func, y, x, var)
+    fitter = Minuit2.JuliaFcn(gaussFunc, y, x, var)
+    Minuit2.fit(fitter, parameters)
+    println("best parameters are ", parameters)
 end
